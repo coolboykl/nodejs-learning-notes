f3699d15c99eeeb3e12e0d0454f20c83032e5f17
Khoo Chen Shiang <khoo.james@gmail.com>
2018-06-08 16:52:56 +0800

    added new better CVS parse in sync

--- SmartAgri_SensorCSVParser/index.js
+++ SmartAgri_SensorCSVParser/index.js
@@ -11,10 +11,20 @@ program
     .option('-f, --filter [filter]', 'to filter the seson on/off data')
     .command('input <inputfile>')
     .description('sensor on-off data in csv file to parse.')
-    .action( (inputfile) => {
-        console.log(`Parsing sensor file =>` + inputfile + ',with filter ->' + program.filter);
+    .action((inputfile) => {
+        //console.log(`Parsing sensor file =>` + inputfile + ',with filter ->' + program.filter);
         let csvfile = fs.readFileSync(inputfile);
-        sensorCSVParser(csvfile, program.filter);
-    });
-program.parse(process.argv); 
+        let parseRecordObj = sensorCSVParser(csvfile);
+        //console.log(JSON.stringify(parseRecordOb));
 
+        for (var index in parseRecordObj.summary) {
+            var sensorData = parseRecordObj.summary[index];
+            if (program.filter && program.filter === sensorData.status) {
+                console.log(sensorData.status + ',' + sensorData.startTime + ',' + sensorData.endTime + ',' + sensorData.duration);
+            } else if (!program.filter) {
+                console.log(sensorData.status + ',' + sensorData.startTime + ',' + sensorData.endTime + ',' + sensorData.duration);
+
+            }
+        }
+    });
+program.parse(process.argv);
--- SmartAgri_SensorCSVParser/test/mixpump_parser_test.js
+++ SmartAgri_SensorCSVParser/test/mixpump_parser_test.js
@@ -4,11 +4,11 @@ const chai = require('chai');
 const fs = require('fs');
 const expect = chai.expect;
 const sensorParser = require('../lib/sensorCSVParser.js');
-const pumpCSV = fs.readFileSync(`${__dirname}/mixpump_apr2018_setC.csv`);
 
-describe('mixpumpParser parsing mixpump_apr2018_setC data', () => {
-    const summaryData = sensorParser(pumpCSV);
-    //console.log('npm test->' + JSON.stringify(summaryData));
+describe('parsing mixpump_apr2018_setC data without filter', () => {
+    const pumpCSV = fs.readFileSync(`${__dirname}/mixpump_apr2018_setC.csv`);
+    const parseRecordObj = sensorParser(pumpCSV);
+    //console.log('npm test->' + JSON.stringify(parseRecordObj));
 
     it('should be a function', () => {
         expect(sensorParser).to.be.a('function');
@@ -16,41 +16,44 @@ describe('mixpumpParser parsing mixpump_apr2018_setC data', () => {
 
     it('should parse mixpump csv file', () => {
        
-        expect(summaryData).to.be.an('object');
-        expect(summaryData).to.have.a.property('summary')
-              .that.is.an('array');
-
-        // First Data should be with Status 0, startime at 2018-04-14 15:26:41, 
+        expect(parseRecordObj).to.be.an('object');
+        expect(parseRecordObj).to.have.a.property('summary')
+              .that.is.an('array').with.lengthOf(8);
     });
 
-    it('checking the first record', () => {
-        expect(summaryData.summary[0]).to.have.a.property('startTime')
+    it('checking the first record, start time, end time, and duration', () => {
+        var firstRecord = parseRecordObj.summary[0];
+        expect(firstRecord).to.have.a.property('startTime')
             .that.is.an('string')
             .and.contains('2018-04-14 15:26:41');
+
+        expect(firstRecord).to.have.a.property('duration')
+            .that.is.an('string')
+            .and.contains('0:16:38');
     })
 });
 
-    //     const book = parseRDF(rdf);
-    //     expect(book).to.be.an('object');
-    //     expect(book).to.have.a.property('id', 132);
-    //     expect(book).to.have.a.property('title','The Art of War');
 
-    //     expect(book).to.have.a.property('authors')
-    //         .that.is.an('array').with.lengthOf(2)
-    //         .and.contains('Sunzi, active 6th century B.C.')
-    //         .and.contains('Giles, Lionel');
+describe('parsing mixpump_apr2018_setA', () => {
+    const pumpCSV = fs.readFileSync(`${__dirname}/mixpump_apr2018_setA.csv`);
+    const parseRecordObj = sensorParser(pumpCSV);
+    //console.log('npm test->' + JSON.stringify(parseRecordObj));
 
-    //     expect(book).to.have.a.property('subjects')
-    //         .that.is.an('array').with.lengthOf(2)
-    //         .and.contains('Military art and science -- Early works to 1800')
-    //         .and.contains('War -- Early works to 1800');
+    it('should parse mixpump csv file', () => {
+       
+        expect(parseRecordObj).to.be.an('object');
+        expect(parseRecordObj).to.have.a.property('summary')
+              .that.is.an('array').with.lengthOf(10);
+    });
 
-    //     expect(book).to.have.a.property('lcc')
-    //         .that.is.an('string')
-    //         .and.contains(book.lcc.toUpperCase());
-    //     expect(['I', 'O', 'W','X','Yaq']).not.to.be.containing(book.lcc);
+    it('checking the first record, start time, end time, and duration', () => {
+        var firstRecord = parseRecordObj.summary[0];
+        expect(firstRecord).to.have.a.property('startTime')
+            .that.is.an('string')
+            .and.contains('2018-04-09 00:10:34');
 
-    //     expect(book).to.have.a.property('downloads')
-    //         .that.is.an('array').with.lengthOf(10)
-    // })
-// });
+        expect(firstRecord).to.have.a.property('duration')
+            .that.is.an('string')
+            .and.contains('0:8:3');
+    })
+});
